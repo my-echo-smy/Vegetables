01ab2df806e96e64a012f54d2132c8e8f4319cc5
simengyuan <2441241367@qq.com>
2020-11-06 09:55:24 +0800

    提交框架

---
+++ src/main/java/com/example/vegetables/VegetablesApplication.java
@@ -0,0 +1,13 @@
+package com.example.vegetables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegetablesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VegetablesApplication.class, args);
+    }
+
+}
